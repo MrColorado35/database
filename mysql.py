8432287571882097e5ff0938511c424455d89e98
MrColorado35 <stan.kaczorkiewicz@yahoo.co.uk>
2020-05-11 20:18:06 +0000

    More tricks with python and MySQL

--- mysql.py
+++ mysql.py
@@ -12,12 +12,44 @@ connection = pymysql.connect(host='localhost',
                             db='Chinook')
 
 try:
-    # Run a query
+    # delete many rows with WHERE IN command:
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        names = ['jum', 'bob']
+        cursor.execute("DELETE FROM Friend WHERE name in (%s, %s)", names )
+        connection.commit()
+
+
+
+    # to delete many
+    # with connection.cursor() as cursor:
+    #     row = cursor.executemany("Delete From Friends Where name = %s ;", ['bob', 'Stan'])
+    #     connection.commit()
+
+        # to delete:
+        # rows =  cursor.execute("DELETE FROM Friends WHERE name ='Bob';")
+        # connection.commit()
+    
+    # update many
+    # with connection.cursor() as cursor:
+    #     rows = [(23, 'Bob'),
+    #             (27, 'Stan'),
+    #             (74, 'Jim')]
+    #     cursor.executemany("UPDATE Friends SET age = %s WHERE name= %s;", rows)
+    #     connection.commit()
+  
+  
+    # # Run a query
+    # with connection.cursor(pymysql.cursors.DictCursor) as cursor:
+    #     row = [("Bob", 30, "1990-09-06 21:21:21"),
+    #           ("Stan", 28, "1991-11-23 17:16:15"),
+    #           ("Jim", 56, "1958-10-10 12:12:25")]
+    #     cursor.executemany("INSERT INTO Friends VALUES (%s, %s, %s);", row)
+    #     connection.commit()
+
+        # result = cursor.fetchall()
+        # print(result)
+        # for row in cursor:
+        #     print(row)
 finally:
     # Close the connection to sql
     connection.close()
